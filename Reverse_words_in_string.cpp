35764f712a309160f8550bd58c302042f4390507
Ankush <ankush270008@gmail.com>
2026-04-24 16:37:24 +0530

    Add files via upload

---
+++ Reverse_words_in_string.cpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+string reverseWords(string s) {
+        int n = s.length();
+        string ans = "";
+        reverse(s.begin(),s.end());
+        for(int i = 0;i<n;i++){
+            string word = "";
+            while(i<n && s[i]!=' '){
+                word += s[i];
+                i++;
+            
+            }
+            reverse(word.begin(),word.end());
+            if(word.length()>0){
+                ans += " " + word;
+            }
+        }
+        return ans.substr(1);
+    }
+int main(){
+    string st = "hello world";
+     string ans = reverseWords(st);
+    cout<<"Reversed string :"<<ans;
+}
