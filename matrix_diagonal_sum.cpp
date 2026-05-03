6a0bbc5043560f5ea204cb584649962e8f8ee317
Ankush <ankush270008@gmail.com>
2026-05-03 19:40:57 +0530

    Add files via upload

---
+++ matrix_diagonal_sum.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+int diagonalSum(int mat[][4], int n)
+{
+    int sum = 0;
+    for (int i = 0; i < n; i++)
+    {
+        for (int j = 0; j < n; j++)
+        {
+            if (i == j)
+            {
+                sum += mat[i][j]; // Primary Diagonal
+            }
+            else if (j == n - i - 1)
+            {
+                sum += mat[i][j]; // Secondary Diagonal
+            }
+        }
+    }
+    return sum;
+}
+int main()
+{
+    int matrix[4][4] = {{1, 2, 3, 4}, {5, 6, 7, 8}, {9, 10, 11, 12}, {13, 14, 15, 16}};
+    int n = 4;
+    cout << diagonalSum(matrix, n) << endl;
+    return 0;
+}
